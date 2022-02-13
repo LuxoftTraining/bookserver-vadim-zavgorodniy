eee16dd6158f8f5500974654b1d9b099f6a1977a
Vadim Zavgorodniy <VTB4046449@corp.dev.vtb>
2022-02-13 20:04:03 +0300

    Add some tests

--- src/test/java/ru/warmouse/coolbooks/StressTest.java
+++ src/test/java/ru/warmouse/coolbooks/StressTest.java
@@ -2,19 +2,56 @@ package ru.warmouse.coolbooks;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.sql.SQLException;
 import java.time.Duration;
 import java.util.List;
+import org.junit.Ignore;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.loadtest4j.LoadTester;
 import org.loadtest4j.Request;
 import org.loadtest4j.Result;
 import org.loadtest4j.drivers.jmeter.JMeterBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
+@SpringBootTest
 public class StressTest {
+    private static final int BOOKS_AMOUNT = 100_000;
     //private static final LoadTester loadTester = LoadTesterFactory.getLoadTester();
 
+    // @Autowired
+    // private BookService bookService;
+
+    @BeforeAll
+    public static void clearDB(@Autowired BookService bookService) throws SQLException {
+        bookService.clear();
+    }
+
     @Test
-    public void test_generate100k_within5sec() {
+    @Order(1)
+    @RepeatedTest(3)
+    @Rollback
+    public void addBooksWarmup() {
+        List<Request> requests = List.of(Request.post("/books/generate/1000")
+                .withHeader("Accept", "application/json"));
+
+        LoadTester loadTester = JMeterBuilder.withUrl("http", "localhost", 8082)
+                .withNumThreads(10).withRampUp(1).build();
+
+        Result result = loadTester.run(requests);
+
+        printReport(result);
+    }
+
+    @RepeatedTest(3)
+    @Order(10)
+    @Transactional
+    public void addBooks() {
         List<Request> requests = List.of(Request.post("/books/generate/100000")
                 .withHeader("Accept", "application/json"));
 
@@ -26,23 +63,68 @@ public class StressTest {
         printReport(result);
 
         assertThat(result.getResponseTime().getPercentile(90))
-                .isLessThanOrEqualTo(Duration.ofSeconds(5));
+                .isLessThanOrEqualTo(Duration.ofSeconds(15));
     }
 
-    @Test
-    public void test_findByKeyword_within20sec() {
-        List<Request> requests = List.of(Request.get("/books/keywords/book")
+    @RepeatedTest(3)
+    @Order(20)
+    @Rollback
+    public void test_findBooks_warmUp() {
+        List<Request> requests = List.of(Request.get("/books/keywords/Title45600")
                 .withHeader("Accept", "application/json"));
 
         LoadTester loadTester = JMeterBuilder.withUrl("http", "localhost", 8082)
-                .withNumThreads(5).withRampUp(1).build();
+                .withNumThreads(1000).withRampUp(1).build();
 
         Result result = loadTester.run(requests);
 
         printReport(result);
+    }
+
+    @RepeatedTest(3)
+    @Order(21)
+    @Rollback
+    public void test_findByOneKeyword_within2sec() {
+        Result result = findBooksTest("Title10045", 10000);
+
+        printReport(result);
 
         assertThat(result.getResponseTime().getPercentile(90))
-                .isLessThanOrEqualTo(Duration.ofSeconds(20));
+                .isLessThanOrEqualTo(Duration.ofMillis(30));
+    }
+
+    @RepeatedTest(3)
+    @Order(22)
+    @Rollback
+    public void test_findByTwoKeyword_within2sec() {
+        Result result = findBooksTest("Title10045%20AuthorSurname12500", 10000);
+
+        printReport(result);
+
+        assertThat(result.getResponseTime().getPercentile(90))
+                .isLessThanOrEqualTo(Duration.ofMillis(30));
+    }
+
+    @Order(23)
+    @RepeatedTest(3)
+    @Rollback
+    public void test_findByThreeKeyword_within2sec() {
+        Result result = findBooksTest("AuthorName70210%20Title10045%20AuthorSurname12500", 10000);
+
+        printReport(result);
+
+        assertThat(result.getResponseTime().getPercentile(90))
+                .isLessThanOrEqualTo(Duration.ofMillis(30));
+    }
+
+    public Result findBooksTest(String pattern, int threadsCount) {
+        List<Request> requests = List.of(Request.get("/books/keywords/" + pattern)
+                .withHeader("Accept", "application/json"));
+
+        LoadTester loadTester = JMeterBuilder.withUrl("http", "localhost", 8082)
+                .withNumThreads(threadsCount).withRampUp(1).build();
+
+        return loadTester.run(requests);
     }
 
     private void printReport(Result result) {
